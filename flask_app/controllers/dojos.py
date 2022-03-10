9c348c1d5e70d26706c344dad51094327be6e1ac
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-03-09 19:05:44 -0700

    Changed to a get_by_id model

--- flask_app/controllers/dojos.py
+++ flask_app/controllers/dojos.py
@@ -1,65 +1,34 @@
 from flask_app import app
-from flask import redirect, render_template,request
+from flask import redirect, render_template, request, session
 from ..models.dojo import Dojo
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/process', methods=['POST'])
-def process():
-    if Dojo.is_valid(request.form):
-        Dojo.save(request.form)
-        return redirect('/results')
+@app.route('/submit', methods=["POST"])
+def submit():
+    print(f"REQUEST FORM {request.form}")
+    if not Dojo.is_valid(request.form):
+        return redirect("/")
+    data = {
+        "name" : request.form["name"],
+        "location" : request.form["location"],
+        "language" : request.form["language"],
+        "comment" : request.form["comment"]
+    }
+    new_dojo_id = Dojo.save(data)
+    print(f" NEW DOJO ID: {new_dojo_id}")
+    return redirect(f'/display/{new_dojo_id}',)
+
+@app.route('/display/<int:id>')
+def display(id):
+    data = {
+        "id": id
+    }
+    return render_template("results.html", dojo = Dojo.get_by_id(data))
+
+@app.route('/destroy')
+def destroy():
+    session.clear()
     return redirect('/')
-
-@app.route('/results')
-def results():
-    return render_template('results.html', dojo = Dojo.get_last())
-
-# @app.route('/books/create',methods=['POST'])
-# def create_book():
-#     Book.save(request.form)
-#     return redirect('/books')
-
-# @app.route('/books/<int:id>')
-# def show_book(id):
-#     data ={
-#         "id":id
-#     }
-#     return render_template("show_book.html", book=Book.get_by_id(data),unfavorited_authors=Author.unfavorited_authors(data))
-
-# @app.route('/join/author',methods=['POST'])
-# def join_author():
-#     data = {
-#         'author_id': request.form['author_id'],
-#         'book_id': request.form['book_id']
-#     }
-#     Author.add_favorite(data)
-#     return redirect(f"/books/{request.form['book_id']}")
-
-# @app.route('/users/new')
-# def new():
-#     return render_template("create.html")
-
-
-# @app.route('/user/edit/<int:id>')
-# def edit(id):
-#     data ={
-#         "id":id
-#     }
-#     return render_template("edit.html", user = User.get_one(data))
-
-
-# @app.route('/user/update',methods=['POST'])
-# def update():
-#     User.update(request.form)
-#     return redirect('/users')
-
-# @app.route('/user/delete/<int:id>')
-# def delete(id):
-#     data = {
-#         'id': id
-#     }
-#     User.delete(data)
-#     return redirect('/users')
--- flask_app/models/dojo.py
+++ flask_app/models/dojo.py
@@ -17,10 +17,18 @@ class Dojo:
         query = "INSERT INTO dojos (name , location, language, comment) VALUES (%(name)s, %(location)s, %(language)s, %(comment)s);"
         return connectToMySQL('dojo_survey_schema').query_db( query, data )
 
+#! cool query that the instructor used to pull the information from the last person to fill out the form
+    # @classmethod
+    # def get_last(cls):
+    #     query = "SELECT * FROM dojos ORDER BY dojos.id DESC LIMIT 1;"
+    #     results = connectToMySQL('dojo_survey_schema').query_db(query)
+    #     return Dojo(results[0])
+
     @classmethod
-    def get_last(cls):
-        query = "SELECT * FROM dojos ORDER BY dojos.id DESC LIMIT 1;"
-        results = connectToMySQL('dojo_survey_schema').query_db(query)
+    def get_by_id(cls, data):
+        query = "SELECT * FROM dojos WHERE id = %(id)s"
+        results = connectToMySQL('dojo_survey_schema').query_db(query, data)
+        print(results)
         return Dojo(results[0])
 
     @staticmethod
@@ -39,93 +47,3 @@ class Dojo:
             is_valid = False
             flash("Comments must be at least 3 characters.")
         return is_valid
-
-    # @classmethod
-    # def get_all(cls):
-    #     query = "SELECT * FROM users;"
-    #     results = connectToMySQL('friendships_schema').query_db(query)
-    #     users = []
-    #     for user in results:
-    #         users.append( cls(user) )
-    #     return users
-
-    # @classmethod
-    # def get_all_friendships(cls):
-    #     query  = "SELECT * FROM users JOIN friendships ON users.id = friendships.user_id JOIN users AS friends ON friendships.friend_id=friends.id;"
-    #     result = connectToMySQL('friendships_schema').query_db(query)
-    #     return result
-
-    # @classmethod
-    # def create_user(cls, data):
-    #     query="INSERT INTO users (first_name, last_name, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, NOW(), NOW());"
-    #     user_id = connectToMySQL('friendships_schema').query_db(query, data)
-    #     return user_id
-
-    # @classmethod
-    # def create_friendship(cls, data):
-    #     query="INSERT INTO friendships (user_id, friend_id, created_at, updated_at) VALUES (%(user_id)s, %(friend_id)s, NOW(), NOW());"
-    #     friendship_id = connectToMySQL('friendships_schema').query_db(query, data)
-    #     return friendship_id
-
-
-    # @classmethod
-    # def get_one(cls,data):
-    #     query  = "SELECT * FROM books WHERE id = %(id)s;"
-    #     result = connectToMySQL('books_schema').query_db(query, data)
-    #     return cls(result[0])
-
-    # @classmethod
-    # def get_by_id(cls,data):
-    #     query = "SELECT * FROM books LEFT JOIN favorites ON books.id = favorites.book_id LEFT JOIN authors ON authors.id = favorites.author_id WHERE books.id = %(id)s;"
-    #     results = connectToMySQL('books_schema').query_db(query,data)
-
-    #     book = cls(results[0])
-
-    #     for row in results:
-    #         if row['authors.id'] == None:
-    #             break
-    #         data = {
-    #             "id": row['authors.id'],
-    #             "first_name": row['first_name'],
-    #             "last_name": row['last_name'],
-    #             'created_at': row['created_at'],
-    #             'updated_at': row['updated_at']
-    #         }
-    #         book.authors_who_favorited.append(author.Author(data))
-    #     return book
-
-    # @classmethod
-    # def unfavorited_books(cls,data):
-    #     query = "SELECT * FROM books WHERE books.id NOT IN ( SELECT book_id FROM favorites WHERE author_id = %(id)s );"
-    #     results = connectToMySQL('books_schema').query_db(query,data)
-    #     books = []
-    #     for row in results:
-    #         books.append(cls(row))
-    #     print(books)
-    #     return books
-    # @classmethod
-    # def dojo_with_members(cls, data ):
-    #     query = "SELECT * FROM books LEFT JOIN authors on books.id = authors.book_id WHERE books.id = %(id)s;"
-    #     results = connectToMySQL('books_schema').query_db(query,data)
-    #     book = cls(results[0])
-    #     for row in results:
-    #         n = {
-    #             'id': row['books.id'],
-    #             'title': row['title'],
-    #             'num_of_pages': row['num_of_pages'],
-    #             'created_at': row['books.created_at'],
-    #             'updated_at': row['books.updated_at']
-    #         }
-    #         book.authors.append(Author(n))
-    #     return book
-
-
-    # @classmethod
-    # def update(cls,data):
-    #     query = "UPDATE users SET first_name=%(fname)s,last_name=%(lname)s,email=%(email)s,updated_at=NOW() WHERE id = %(id)s;"
-    #     return connectToMySQL('books_schema').query_db(query,data)
-
-    # @classmethod
-    # def delete(cls,data):
-    #     query  = "DELETE FROM users WHERE id = %(id)s;"
-    #     return connectToMySQL('books_schema').query_db(query,data)
